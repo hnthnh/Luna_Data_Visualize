7fbc2f88452827b5c4ef873889735fc223be4804
Hoang The Anh <2001202008@hufi.edu.vn>
2024-10-02 11:05:41 +0700

    Update language.py

--- language.py
+++ language.py
@@ -2,29 +2,36 @@
 
 translations = {
     "English": {
-        "btn_browseZIP": "Browse ZIP File",
         "btn_browseFolder": "Browse folder",
         "btn_language_Eng": "English",
         "btn_language_Jap": "Japanese",
         "btn_start": "START",
-        "range_label":"Setting the range of numbers",
-        "btn_search": "Search",
-        "search_label":"Graph Items",
         "btn_add": "Add",
-        "label_GraphLimit": "Limit for Graph",
-        "label_ExpanNumber": "Expan Number",
-        "label_version": "Version"
+        "btn_delete": "Delete",
+
+        "label_GraphItem": "Graph Items",
+        "label_UpperLimit": "Upper Limit",
+        "label_LowerLimit": "Lower Limit",
+        "label_ExpanNumber": "Expansion Number",
+        "label_Expan": "Expan Number",
+
+        "tree_name":"Column Name"
     },
     "Japanese": {
-        "btn_browseZIP": "ZIPファイルを参照",
-        "btn_browseFolder": "フォルダーを参照",
-        "btn_language_Eng": "英語",
-        "btn_language_Jap": "日本語",
-        "btn_start": "スタート",
-        "btn_search": "検索",
-        "label_GraphLimit": "グラフの制限",
-        "label_ExpanNumber": "拡張番号",
-        "label_version": "バージョン"
+        "btn_browseFolder": "Browse folder",
+        "btn_language_Eng": "English",
+        "btn_language_Jap": "Japanese",
+        "btn_start": "START",
+        "btn_add": "Add",
+        "btn_delete": "Delete",
+
+        "label_GraphItem": "Graph Items",
+        "label_UpperLimit": "Upper Limit",
+        "label_LowerLimit": "Lower Limit",
+        "label_ExpanNumber": "Expansion Number",
+        "label_Expan": "Expan Number",
+
+        "tree_name":"Column Name"
     }
 }
 
